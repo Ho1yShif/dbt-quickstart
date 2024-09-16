460f23b76e36714ec65a928a9bb884a6c3d7f2ae
Shifra Isaacs <shifra@sigmacomputing.com>
2024-09-16 00:13:09 +0000

    Build stripe table and fact orders table

---
+++ models/staging/stripe/stg_stripe__payments.sql
@@ -0,0 +1,10 @@
+SELECT
+    id AS customer_id,
+    orderid AS order_id,
+    paymentmethod AS payment_method,
+    status,
+    --Raw amount is stored in cents
+    amount / 100 AS amount_dollars,
+    created AS created_at
+FROM `dbt-tutorial.stripe.payment`
+-- Needed to use this reference instead of the tutorial's recommended raw.stripe.payment
